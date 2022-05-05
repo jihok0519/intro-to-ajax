e37c1b674ed0a001dc262c40e3884428df3b5e38
Jiho Kim <jihokim@Jihos-MacBook-Pro.local>
2022-05-05 16:47:10 -0400

    work in progress: we can perform ajax. minimum functionality

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+// How to request data from an API using JavaScript and jQuery.
 
-const promise = $.ajax({
-    url:'https://www.omdbapi.com/?apikey=53aa2cd6&t=Frozen'
-});
+// There are many ways to do this.
+// 1. jQuery's $.ajax() function
+// 2. The Browser's fetch() function
+// 3. Using the Axios http client library
 
-promise.then(
-  (data) => {
-   console.log(data);
-  },
-  (error) => {
-   console.log('bad request: ', error);
-  }
-);
+const URL = "https://www.omdbapi.com/?apikey=dbce1723&t=DieHard";
+
+function handleGetData() {
+    $.ajax(URL).then(function(data) {
+        console.log("movie data is ready.");
+        console.log(data);
+    }, function(error) {
+        console.log("something is wrong...");
+        console.log(error);
+    })
+}
+
+
+
+// below is, give or take, similar to the one on top
+ 
+// const promise = $.ajax({
+//     url:'https://www.omdbapi.com/?apikey=53aa2cd6&t=Frozen'
+// });
+
+// promise.then(
+//   (data) => {
+//    console.log(data);
+//   },
+//   (error) => {
+//    console.log('bad request: ', error);
+//   }
+// );
+
